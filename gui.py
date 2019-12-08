a5a1b6b33c036f3528b5d5ad65fe49c868eb93fc
Tomasz Lisek <tlisek@student.agh.edu.pl>
2019-12-08 20:19:53 +0100

    Działa dużo rzeczy

---
+++ gui.py
@@ -0,0 +1,97 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Kolejki")
+
+root.config(bg='#ECECEC')
+root.resizable(0, 0) #Don't allow resizing in the x or y direction
+
+def runBtnCallback():
+    print "Run"
+
+
+lambda_1_0 = tk.IntVar(root, value=1/2.0)
+lambda_2_0 = tk.IntVar(root, value=1/2.0)
+
+
+mi_1 = tk.IntVar(root, value=1/2.0)
+mi_2 = tk.IntVar(root, value=1/2.0)
+
+
+def runBtnCallback():
+    print 'Run'
+
+lambdas_frame = ttk.Frame(root, relief='raised', borderwidth=1)
+lambdas_frame.pack(fill='both', expand=False)
+
+
+lambdas_frame_row0 = ttk.Frame(lambdas_frame)
+lambdas_frame_row0.pack(fill='both', expand=False)
+
+lambda_label = ttk.Label(lambdas_frame_row0, text="Lambda startowe")
+lambda_label.pack(side='left', fill='both', padx=5, pady=5)
+
+
+lambdas_frame_row1 = ttk.Frame(lambdas_frame)
+lambdas_frame_row1.pack(fill='both', expand=False)
+
+lambda_1_0_label = ttk.Label(lambdas_frame_row1, text="Klasa 1")
+lambda_1_0_label.pack(side='left', fill='both', padx=5, pady=5)
+
+lambda_1_0_val = ttk.Entry(lambdas_frame_row1, textvariable=lambda_1_0)
+lambda_1_0_val.pack(expand=True, fill='both', padx=5, pady=5)
+
+
+lambdas_frame_row2 = ttk.Frame(lambdas_frame)
+lambdas_frame_row2.pack(fill='both', expand=False)
+
+lambda_2_0_label = ttk.Label(lambdas_frame_row2, text="Klasa 2")
+lambda_2_0_label.pack(side='left', fill='both', padx=5, pady=5)
+
+lambda_2_0_val = ttk.Entry(lambdas_frame_row2, textvariable=lambda_2_0)
+lambda_2_0_val.pack(expand=True, fill='both', padx=5, pady=5)
+
+
+
+
+mi_frame = ttk.Frame(root, relief='raised', borderwidth=1)
+mi_frame.pack(fill='both', expand=False)
+
+
+mi_frame_row0 = ttk.Frame(mi_frame)
+mi_frame_row0.pack(fill='both', expand=False)
+
+mi_label = ttk.Label(mi_frame_row0, text="Mi startowe")
+mi_label.pack(side='left', fill='both', padx=5, pady=5)
+
+
+mi_frame_row1 = ttk.Frame(mi_frame)
+mi_frame_row1.pack(fill='both', expand=False)
+
+mi_1_label = ttk.Label(mi_frame_row1, text="System 1")
+mi_1_label.pack(side='left', fill='both', padx=5, pady=5)
+
+mi_1_val = ttk.Entry(mi_frame_row1, textvariable=mi_1)
+mi_1_val.pack(expand=True, fill='both', padx=5, pady=5)
+
+
+mi_frame_row2 = ttk.Frame(mi_frame)
+mi_frame_row2.pack(fill='both', expand=False)
+
+mi_2_label = ttk.Label(mi_frame_row2, text="System 2")
+mi_2_label.pack(side='left', fill='both', padx=5, pady=5)
+
+mi_2_val = ttk.Entry(mi_frame_row2, textvariable=mi_2)
+mi_2_val.pack(expand=True, fill='both', padx=5, pady=5)
+
+
+
+frame_bottom = ttk.Frame(root)
+frame_bottom.pack(fill='both', expand=False, side='bottom')
+
+runButton = ttk.Button(frame_bottom, text="Run", command=runBtnCallback)
+runButton.pack(side='right', padx=5, pady=5)
+
+
+root.mainloop()
---
+++ solve_K.py
@@ -0,0 +1,61 @@
+from solve_lambda import *
+import math
+# System i
+# Klasa r
+
+def calc_K_FIFO(m_i, ro_i_r, ro_i):
+    sum = 0
+    for k in range(0,m_i):
+        sum += 1.0 * ((m_i*ro_i)**k) / math.factorial(k)
+
+    return m_i*ro_i_r + ro_i_r/(1-ro_i) * ((m_i*ro_i)**m_i) / ( math.factorial(m_i)*(1-ro_i) ) * 1/( sum + (((m_i*ro_i)**m_i)/(math.factorial(m_i)) ) * (1/(1-ro_i)) )
+
+def calc_K_IS(lam_i_r,mi_i):
+    return lam_i_r/mi_i
+
+
+
+# System 1 - Typ IS
+print "System 1:"
+# Klasa 1
+K_1_1 = calc_K_IS(lam[1][1], mi_1)
+
+# Klasa 2
+K_1_2 = calc_K_IS(lam[1][2], mi_1)
+
+# Klasa 3
+K_1_3 = calc_K_IS(lam[1][3], mi_1)
+
+# Klasa 4
+K_1_4 = calc_K_IS(lam[1][4], mi_1)
+
+# Klasa 5
+K_1_5 = calc_K_IS(lam[1][5], mi_1)
+
+print [K_1_1, K_1_2, K_1_3, K_1_4, K_1_5]
+
+
+# System 3 - Typ FIFO
+print "System 3:"
+
+# Klasa 1
+ro_3_1 = lam[3][1]/(m_3*mi_3)
+
+# Klasa 2
+ro_3_2 = lam[3][2]/(m_3*mi_3)
+
+# Klasa 3
+ro_3_3 = lam[3][3]/(m_3*mi_3)
+
+# Klasa 4
+# W systemie 3 nie ma klasy 4
+
+# Klasa 5
+# W systemie 3 nie ma klasy 5
+
+ro_3 = (ro_3_1 + ro_3_2 + ro_3_3) / 3.0
+
+K_3_1 = calc_K_FIFO(m_3, ro_3_1, ro_3)
+K_3_2 = calc_K_FIFO(m_3, ro_3_2, ro_3)
+K_3_3 = calc_K_FIFO(m_3, ro_3_3, ro_3)
+print [K_3_1, K_3_2, K_3_3]
