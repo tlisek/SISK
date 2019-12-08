f6d29cd0cf6c0030d1214ad8335dd00ff0666604
Tomasz Lisek <tlisek@student.agh.edu.pl>
2019-12-08 18:10:09 +0100

    C2

--- run.py
+++ run.py
@@ -1,16 +1,36 @@
+import numpy as np
+
 # System i
 # Klasa r
 
 #System 1
-
+lam_0_1 = 1/15
 
 #System 2
 
+lam_0_2 = 1/15
+A_list =[
+    [0.1, 0.1, 1, -1],
+    [0.6, -1, 0, 0],
+    [0.3, 0.9, -1, 0],
+    [1, 0, 0, 0]
+]
+
+b_list = [[0],[0],[0],[lam_0_2]]
+
+A = np.array(A_list)
+b = np.array(b_list)
+X = np.linalg.inv(A).dot(b)
+
+print X
 
 #System 3
+lam_0_1 = 1/15
 
 
 #System 4
+lam_0_1 = 1/15
 
 
 #System 5
+lam_0_1 = 1/15
