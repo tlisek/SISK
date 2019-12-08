5177751d4dd1f05cfcf3757a3fe65ebbc4ee719b
Tomasz Lisek <tlisek@student.agh.edu.pl>
2019-12-08 18:30:18 +0100

    System1

--- run.py
+++ run.py
@@ -4,7 +4,25 @@ import numpy as np
 # Klasa r
 
 #System 1
-lam_0_1 = 1/15
+lam_0_1 = 1
+A_list =[
+    [-1, 0, 0, 0, 0],
+    [0.6, -1, 0, 0, 0],
+    [0.3, 0.9, -1, 0.05, 0.05],
+    [0.05, 0.05, 0.5, -1, 0],
+    [0.05, 0.05, 0.5, 0, -1],
+]
+b_list = [[lam_0_1], [0],[0],[0],[0]]
+A = np.array(A_list)
+b = np.array(b_list)
+lam_1 = np.linalg.inv(A).dot(b)
+print "\n"
+print "System 1"
+print "Lambda"
+print lam_1
+
+#System 2
+lam_0_2 = 1
 A_list =[
     [0.1, 0.1, 1, -1],
     [0.6, -1, 0, 0],
@@ -13,20 +31,13 @@ A_list =[
 ]
 b_list = [[0],[0],[0],[lam_0_2]]
 
-#System 2
-lam_0_2 = 1
-A_list =[
-    [-1, 0.1, 1, -1],
-    [0.6, -1, 0, 0],
-    [0.3, 0.9, -1, 0.1],
-    [1, 0, 0, 0]
-]
-b_list = [[0],[0],[0],[lam_0_2]]
-
 A = np.array(A_list)
 b = np.array(b_list)
-X_2 = np.linalg.inv(A).dot(b)
-print X_2
+lam_2 = np.linalg.inv(A).dot(b)
+print "\n"
+print "System 2"
+print "Lambda"
+print lam_2
 
 #System 3
 lam_0_3 = 1
@@ -40,8 +51,11 @@ b_list = [[0],[0],[0],[lam_0_2]]
 
 A = np.array(A_list)
 b = np.array(b_list)
-X_3 = np.linalg.inv(A).dot(b)
-print X_3
+lam_3 = np.linalg.inv(A).dot(b)
+print "\n"
+print "System 3"
+print "Lambda"
+print lam_3
 
 
 #System 4
